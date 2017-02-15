7c7af9570e17bd9e32782af42368cabab6a15282
bfleyshe <bfleyshe@ualberta.ca>
2017-02-14 19:13:04 -0700

    Branch updated with Jtesting

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -1,8 +1,90 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import android.test.ActivityInstrumentationTestCase2;
+
+import java.util.ArrayList;
+
+
 /**
  * Created by bfleyshe on 2/14/17.
  */
 
-public class TweetListTest {
+public class TweetListTest extends ActivityInstrumentationTestCase2{
+
+    public TweetListTest(){
+        super(LonelyTwitterActivity.class);
+    }
+
+    public void testAddTweet(){
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("test tweet");
+
+        tweets.addTweet(tweet);
+        assertTrue(tweets.hasTweet(tweet));
+        tweets.addTweet(tweet);
+        assertEquals(tweets.getCount(), 1);
+
+    }
+
+    public void testGetTweet(){
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("test tweet");
+
+        tweets.addTweet(tweet);
+        Tweet returnedTweet = tweets.getTweet(0);
+
+        assertEquals(returnedTweet.getMessage(), tweet.getMessage());
+        assertEquals(returnedTweet.getDate(), tweet.getDate());
+    }
+
+    public void testHasTweet(){
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("test tweet");
+        Tweet secondTweet = new NormalTweet("test tweet");
+
+        tweets.addTweet(tweet);
+        tweets.addTweet(secondTweet);
+        assertEquals(tweet, secondTweet);
+    }
+
+    public void testGetTweets(){
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("test tweet");
+        Tweet secondTweet = new NormalTweet("test tweet two");
+
+        tweets.addTweet(secondTweet);
+        tweets.addTweet(tweet);
+
+        ArrayList<Tweet> newTweets = tweets.getTweets();
+        assertNotSame(tweets, newTweets);
+    }
+
+    public void testDeleteTweet(){
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("last tweet");
+
+        tweets.addTweet(tweet);
+        tweets.delete(tweet);
+
+        assertFalse(tweets.hasTweet(tweet));
+
+    }
+
+    public void testStrings(){
+        assertEquals("'test' should be 'test'", "test", "test");
+        assertTrue("'test' should start with 't'", "test".startsWith("t"));
+    }
+
+    public void testGetCount(){
+        TweetList tweets = new TweetList();
+        Tweet tweet = new NormalTweet("test tweet");
+        Tweet anotherTweet = new NormalTweet("also tweet");
+
+        tweets.addTweet(tweet);
+        assertEquals(1, tweets.getCount());
+        tweets.addTweet(anotherTweet);
+        assertEquals(2, tweets.getCount());
+        tweets.delete(tweet);
+        assertEquals(1, tweets.getCount());
+    }
 }
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -1,8 +1,62 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import java.util.ArrayList;
+
 /**
  * Created by bfleyshe on 2/14/17.
  */
 
 public class TweetList {
+
+    private ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+
+    public void addTweet(Tweet tweet){
+
+        if(this.hasTweet(tweet)){
+            throw new IllegalArgumentException();
+        }
+
+        tweets.add(tweet);
+    }
+
+    public boolean hasTweet(Tweet tweet){
+
+        for(int i = 0; i < this.getCount(); i++) {
+            for (int j = 0; i < this.getCount(); i++) {
+                if (tweets.get(i) == tweets.get(j)) {
+                    return true;
+                }
+            }
+        }
+
+        return tweets.contains(tweet);
+    }
+
+    public void delete(Tweet tweet){
+
+        tweets.remove(tweet);
+    }
+
+    public Tweet getTweet(int index){
+
+        return tweets.get(index);
+    }
+
+    public ArrayList<Tweet> getTweets(){// assumes that chronological order by order added into the list
+        return tweets;
+    }
+
+    public int getCount(){
+
+        return tweets.size();
+    }
+
+    public int compare(Tweet tweet1, Tweet tweet2){
+        if(tweet1.getDate()==tweet2.getDate())
+            return 0;
+        else if(tweet1.getDate().before(tweet2.getDate()))
+            return 1;
+        else
+            return -1;
+    }
 }
